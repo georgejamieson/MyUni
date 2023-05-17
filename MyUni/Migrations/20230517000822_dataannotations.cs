9c3bbf6e56b2491a5adba84551d6517b17fe28ef
George Jamieson <ac111480@avcol.school.nz>
2023-05-17 13:10:53 +1200

    adding more data annotations

---
+++ MyUni/Migrations/20230517000822_dataannotations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyUni.Migrations
+{
+    /// <inheritdoc />
+    public partial class dataannotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- MyUni/Models/Student.cs
+++ MyUni/Models/Student.cs
@@ -1,21 +1,38 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyUni.Models
 {
     public class Student
     {
         public int StudentId { get; set; }
-       
-        public string LastName { get; set; }
+
         [Display(Name = "Last Name")]
-        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
         [Display(Name = "First Name")]
-        public DateTime EnrollmentDate { get; set; }
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}",  ApplyFormatInEditMode = true)]
+        public string FirstName { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Full Name")]
+        public string FullName {
+            get{
+                return (FirstName + " " + LastName);
+            } 
+        }
+        
+            
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         [Display(Name = "Enrollment Date")]
-        public DateTime DOB { get; set; }
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}" , ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime EnrollmentDate { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+       
 
         public ICollection <Enrollment> Enrollements { get; set; }
         
